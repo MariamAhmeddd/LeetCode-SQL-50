4281dbff60003f297ce822b6400048b92e22a2c0
Mariam Ahmed <100625724+MariamAhmeddd@users.noreply.github.com>
2024-08-18 14:32:32 +0300

    Update Product Sales Analysis III.sql

--- Product Sales Analysis III.sql
+++ Product Sales Analysis III.sql
@@ -1,5 +1,7 @@
 # Write your MySQL query statement below
-select product_id, min(year)as first_year,quantity,price 
+select product_id, year as first_year, quantity, price
 from Sales
-group by product_id, quantity,price;
-   
+where (product_id, year) in
+   (select product_id, min(year) 
+    from Sales
+    group by product_id);
